e597435b420dd3410e3bde1a5bf97ae9b00c09d9
Lawrence Tan <junhao.lawrence.tan@merck.com>
2019-04-12 14:09:46 +0800

    Clear and Add TODOs

--- app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivity.kt
@@ -2,16 +2,15 @@ package com.raywenderlich.android.tipcalculator
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 
 const val HUNDRED_PERCENT = 100.00
 
-class MainActivity : AppCompatActivity(), View.OnClickListener, TextWatcher {
+//TODO #7: Implements Button Listener
+//TODO #14: Implements TextWatcher
+class MainActivity : AppCompatActivity() {
 
     // Top Section
     private lateinit var expensePerPersonTextView: TextView
@@ -38,77 +37,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, TextWatcher {
     }
 
     private fun initViews() {
-        expensePerPersonTextView = findViewById(R.id.expensePerPersonTextView)
-        billEditText = findViewById(R.id.billEditText)
-        billEditText.addTextChangedListener(this)
-
-        addTipButton = findViewById(R.id.addTipButton)
-        addTipButton.setOnClickListener(this)
-        tipTextView = findViewById(R.id.tipTextView)
-        subtractTipButton = findViewById(R.id.subtractTipButton)
-        subtractTipButton.setOnClickListener(this)
-
-        addPeopleButton = findViewById(R.id.addPeopleButton)
-        addPeopleButton.setOnClickListener(this)
-        numberOfPeopleTextView = findViewById(R.id.numberOfPeopleTextView)
-        subtractPeopleButton = findViewById(R.id.subtractPeopleButton)
-        subtractPeopleButton.setOnClickListener(this)
+        //TODO #6: Connect the Views
+        //TODO #8: Bind Buttons to Listener
+        //TODO #15: Bind EditText to TextWatcher
     }
 
     private fun calculateExpense() {
-
-        val totalBill = billEditText.text.toString().toDouble()
-
-        val totalExpense = ((HUNDRED_PERCENT + tipPercent) / HUNDRED_PERCENT) * totalBill
-        val individualExpense = totalExpense / numberOfPeople
-
-        expensePerPersonTextView.text = String.format("$%.2f", individualExpense)
-
+        //TODO #17: Implement and call calculateExpense as required
     }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.addTipButton -> incrementTip()
-            R.id.subtractTipButton -> decrementTip()
-            R.id.addPeopleButton -> incrementPeople()
-            R.id.subtractPeopleButton -> decrementPeople()
-        }
-    }
+    //TODO #9: Override onClick and implement a switch case to handle multiple buttons
 
     private fun incrementTip() {
-        tipPercent += 5
-        tipTextView.text = String.format("%d%%", tipPercent)
-        calculateExpense()
+        //TODO #10: Implement Tip Increment
     }
 
     private fun decrementTip() {
-        if (tipPercent != 0) {
-            tipPercent -= 5
-            tipTextView.text = String.format("%d%%", tipPercent)
-            calculateExpense()
-        }
+        //TODO #11: Implement Tip Decrement
     }
 
     private fun incrementPeople() {
-        numberOfPeople += 1
-        numberOfPeopleTextView.text = numberOfPeople.toString()
-        calculateExpense()
+        //TODO #12: Implement People Increment
     }
 
     private fun decrementPeople() {
-        if (numberOfPeople != 0) {
-            numberOfPeople -= 1
-            numberOfPeopleTextView.text = numberOfPeople.toString()
-            calculateExpense()
-        }
+        //TODO #13: Implement People Decrement
     }
 
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        if (!billEditText.text.isEmpty()) {
-            calculateExpense()
-        }
-    }
-
-    override fun afterTextChanged(s: Editable?) {}
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+    //TODO #16: Implement onTextChanged, beforeTextChanged & afterTextChanged
 }
